073a91acaad282854481344148ec4bff9e6b1d3c
grvkmrpandit <grvkmrpandit@gmail.com>
2020-09-14 20:54:10 +0530

    facebook messenger UI

---
+++ README.md
@@ -0,0 +1,19 @@
+# Facebook Messenger App UI
+
+![Facebook Messenger App UI](https://user-images.githubusercontent.com/16510597/86947981-bc704f00-c176-11ea-8355-b35f698be928.jpg)
+
+
+A new Flutter project.
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+
+For help getting started with Flutter, view our
+[online documentation](https://flutter.dev/docs), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/constant/data.dart
@@ -0,0 +1,203 @@
+// users story list
+List userStories = [
+  {
+    "id": 1,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": true,
+  },
+  {
+    "id": 2,
+    "name": "shivy",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": false,
+  },
+  {
+    "id": 3,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": false,
+  },
+  {
+    "id": 4,
+    "name": "Navya",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": true,
+  },
+  {
+    "id": 5,
+    "name": "Sneha",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": false,
+  },
+  {
+    "id": 6,
+    "name": "siri",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": true,
+  }
+];
+
+// users message list
+List userMessages = [
+  {
+    "id": 1,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": true,
+    "message": "How are you doing?",
+    "created_at": "1:00 pm"
+  },
+  {
+    "id": 2,
+    "name": "shivy",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": false,
+    "message": "Long time no see!!",
+    "created_at": "12:00 am"
+  },
+  {
+    "id": 3,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": true,
+    "message": "Glad to know you in person!",
+    "created_at": "3:30 pm"
+  },
+  {
+    "id": 4,
+    "name": "Navya",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": false,
+    "message": "I'm doing fine and how about you?",
+    "created_at": "9:00 am"
+  },
+  {
+    "id": 5,
+    "name": "Sneha",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": false,
+    "message": "What is your real name?",
+    "created_at": "11:25 am"
+  },
+  {
+    "id": 6,
+    "name": "siri",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": true,
+    "story": true,
+    "message": "I'm happy to be your friend",
+    "created_at": "10:00 am"
+  },
+  {
+    "id": 7,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": false,
+    "message": "Thanks for your help.",
+    "created_at": "2:34 pm"
+  },
+  {
+    "id": 8,
+    "name": "Alina",
+    "img": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+    "online": false,
+    "story": true,
+    "message": "I just arrived home.",
+    "created_at": "1:12 am"
+  }
+];
+
+// list of messages
+List messages = [
+  {
+    "isMe": true,
+    "messageType": 1,
+    "message": "Hey babes",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": true,
+    "messageType": 2,
+    "message": "kisses",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": true,
+    "messageType": 3,
+    "message": "wanna hava  chat",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 1,
+    "message": "Lets do it",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 2,
+    "message": "yaaaa",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 3,
+    "message": "So what are u wearing today",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": true,
+    "messageType": 1,
+    "message": "I am wearing a pink lingerie",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": true,
+    "messageType": 3,
+    "message": " hmmmmm and u",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 1,
+    "message": "I am currently wearing a red gown",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 2,
+    "message": "wanna have fun tonight",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 3,
+    "message": "Tonight at 10 pm",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": true,
+    "messageType": 4,
+    "message": "Oh hahahah good",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  },
+  {
+    "isMe": false,
+    "messageType": 4,
+    "message": "great",
+    "profileImg": "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+  }
+];
